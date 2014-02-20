90d46e2d19edd4f2fa3c9397939a2dfd551e1434
epicodus <student@epicodus.com>
2014-02-20 09:41:07 -0800

    first commit

---
+++ spec/specs.js
@@ -0,0 +1,11 @@
+describe('factorial', function() {
+  it('multiplies a number by all of the positive integers less than that number', function(){
+    factorial("3").should.equal("6");
+  });
+});
+
+// describe('function name', function() {
+//   it('what you want the function to do', function() {
+//     caps("input").should.equal("output");
+//   });
+// });
